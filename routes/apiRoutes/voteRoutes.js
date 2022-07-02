4eb439523ddc67f8161fd9bf3374785ff4b71758
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-01 22:45:41 -0500

    typo

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -28,13 +28,12 @@ router.post("/vote", ({ body }, res) => {
 });
 
 //tally votes
-router.get('/votes'), (req, res) => {
-const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+router.get("/votes", (req, res) => {
+  const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
 FROM votes
 LEFT JOIN candidates ON votes.candidate_id = candidates.id
 LEFT JOIN parties ON candidates.party_id = parties.id
 GROUP BY candidate_id ORDER BY count DESC`;
-
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -46,7 +45,5 @@ GROUP BY candidate_id ORDER BY count DESC`;
     });
   });
 });
-
-
 
 module.exports = router;
